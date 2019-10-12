d78ac7c656475c6b783a189d1826100a555f8262
apo11oCreed <nelson.ncdesigns@gmail.com>
2019-10-12 14:14:19 -0400

    Beginning build of carousel width

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const respSlideshow = document.getElementById("responsive_slideshow");
+let respSlideshow = document.getElementById("responsive_slideshow");
 const data = "../data/data.json";
 
 let request = new XMLHttpRequest();
@@ -8,9 +8,20 @@ request.send();
 request.onload = function() {
   let slideInstancesObject = request.response;
   let slideInstancesList = slideInstancesObject.slideshow;
-
-  for (let i = 0; i < slideInstancesList.length; i++) {
-    let sliderContainer = document.createElement("div");
-    respSlideshow.appendChild(sliderContainer).setAttribute("id", "slide" + i);
-  }
+  renderSlides(slideInstancesList, respSlideshow);
+  getDimensions(slideInstancesList);
 };
+
+function renderSlides(slides,container){
+for (let i = 0; i < slides.length; i++) {
+  container.innerHTML += '<img id="img_' + i + '" src="' + slides[i].src + '" alt="' + slides[i].name + '" />';
+}
+}
+
+function getDimensions(slides) {
+  for (let i = 0; i < slides.length; i++) {
+    var imgWidth = slides[i].src.naturalWidth;
+    var imgHeight = slides[i].src.naturalHeight;
+    console.log(imgWidth + " " + imgHeight);
+  }
+}
