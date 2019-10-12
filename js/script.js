59aff0af402234d79fe44cc75ce2522df3b8d652
apo11oCreed <nelson.ncdesigns@gmail.com>
2019-10-12 12:40:47 -0400

    Confirmed data source linked up

--- js/script.js
+++ js/script.js
@@ -1,2 +1,16 @@
-var data = JSON.stringify('../data/data.json');
-//alert(data);
+const respSlideshow = document.getElementById("responsive_slideshow");
+const data = "../data/data.json";
+
+let request = new XMLHttpRequest();
+request.open("GET", data);
+request.responseType = "json";
+request.send();
+request.onload = function() {
+  let slideInstancesObject = request.response;
+  let slideInstancesList = slideInstancesObject.slideshow;
+
+  for (let i = 0; i < slideInstancesList.length; i++) {
+    let sliderContainer = document.createElement("div");
+    respSlideshow.appendChild(sliderContainer).setAttribute("id", "slide" + i);
+  }
+};
