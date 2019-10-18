f74bc8675fb8094fb2306715878533376d5b8292
Nelson Correia <nelson.ncdesigns@gmail.com>
2019-10-18 05:54:36 +0000

    Looping mech applied

--- js/script.js
+++ js/script.js
@@ -13,7 +13,8 @@ let marginLeftPosition;
 let img;
 
 var highlightIndex;
-
+var imgWidthRuleUnitValue;
+var imgWidthRuleUnit;
 let request = new XMLHttpRequest();
 request.open("GET", data);
 request.responseType = "json";
@@ -21,10 +22,13 @@ request.send();
 
 request.onload = function() {
   slideInstancesList = request.response.slideshow;
+  var slideWidthRule = new RegExp('[0-9]{1,}vw');
 
   renderSlides(slideInstancesList, respSlideshow, function() {
     let respSlideshowNew = document.getElementById("responsive_slideshow");
     img = respSlideshowNew.querySelectorAll("img");
+    var imgWidthProps = GetStyle('.imgWidth');
+    imgWidthRuleUnitValue = (imgWidthProps.replace(/[^0-9]{1,}/g, ""));
 
     let pageButtons = document.querySelectorAll(".page");
     let imgWidthsArray = [];
@@ -49,7 +53,7 @@ function renderSlides(slides, container, callback) {
 }
 
 function calcSlideShowWidth(slideArray, callback) {
-  slideShowSlideWidth = .container.slideshow# responsive_slideshow
+  respSlideshow.style.width = slideArray.length * imgWidthRuleUnitValue + "vw";
   callback();
 }
 
@@ -60,45 +64,45 @@ function controlsBuilder() {
   img = respSlideshow.querySelectorAll("img");
 
   pageButtons.forEach(function(currentValue, index, arr) {
-        currentValue.addEventListener('click', function() {
+    currentValue.addEventListener('click', function() {
 
-            if (currentValue.classList.contains("page")) {
-              respSlideshow.style.marginLeft = "-" + (index - 2) * 100 + "vw";
-              marginLeftPosition = respSlideshow.style.marginLeft;
-            }
-            else if (currentValue.classList.contains("previous")) {
-              respSlideshowCurrentMargin = respSlideshow.style.marginLeft;
-              if (respSlideshowCurrentMargin && respSlideshowCurrentMargin != "0vw") {
-                respSlideshow.style.marginLeft = (Number(respSlideshowCurrentMargin.replace(/[vw]/g, '')) + 100).toString() + "vw";
-                marginLeftPosition = respSlideshow.style.marginLeft;
+      if (currentValue.classList.contains("page")) {
+        respSlideshow.style.marginLeft = "-" + (index - 2) * imgWidthRuleUnitValue + "vw";
+        marginLeftPosition = respSlideshow.style.marginLeft;
+      }
+      else if (currentValue.classList.contains("previous")) {
+        respSlideshowCurrentMargin = respSlideshow.style.marginLeft;
+        if (respSlideshowCurrentMargin && respSlideshowCurrentMargin != "0vw") {
+          respSlideshow.style.marginLeft = (Number(respSlideshowCurrentMargin.replace(/[vw]/g, '')) + imgWidthRuleUnitValue).toString() + "vw";
+          marginLeftPosition = respSlideshow.style.marginLeft;
 
-              }
-              else {
-                respSlideshow.style.marginLeft = "-" + ((img.length - 1) * 100) + "vw";
-                marginLeftPosition = respSlideshow.style.marginLeft;
-              }
-            }
-            else {
-              if (currentValue.classList.contains("next")) {
-                respSlideshowCurrentMargin = respSlideshow.style.marginLeft;
-                if (respSlideshowCurrentMargin != "-" + ((img.length - 1) * 100).toString() + "vw") {
-                  console.log((img.length - 1) * 100));
-                respSlideshow.style.marginLeft = (Number(respSlideshowCurrentMargin.replace(/[vw]/g, '')) - 100).toString() + "vw";
-                marginLeftPosition = respSlideshow.style.marginLeft;
+        }
+        else {
+          respSlideshow.style.marginLeft = "-" + ((img.length - 1) * imgWidthRuleUnitValue) + "vw";
+          marginLeftPosition = respSlideshow.style.marginLeft;
+        }
+      }
+      else {
+        if (currentValue.classList.contains("next")) {
+          respSlideshowCurrentMargin = respSlideshow.style.marginLeft;
+          if (respSlideshowCurrentMargin != "-" + ((img.length - 1) * imgWidthRuleUnitValue).toString() + "vw") {
+            respSlideshow.style.marginLeft = (Number(respSlideshowCurrentMargin.replace(/[vw]/g, '')) - imgWidthRuleUnitValue).toString() + "vw";
+            marginLeftPosition = respSlideshow.style.marginLeft;
 
-              }
-              else {
-                respSlideshow.style.marginLeft = "0vw";
-                marginLeftPosition = respSlideshow.style.marginLeft;
-              }
-            }
-            }
-            if (marginLeftPosition) {
-              marginLeftPosition = marginLeftPosition.replace(/[-vw]/g, '');
-              highlightIndex = marginLeftPosition / 100 + 2;
-              highlighterIndex(highlightIndex);
-            }
-            });});
+          }
+          else {
+            respSlideshow.style.marginLeft = "0vw";
+            marginLeftPosition = respSlideshow.style.marginLeft;
+          }
+        }
+      }
+      if (marginLeftPosition) {
+        marginLeftPosition = marginLeftPosition.replace(/[-vw]/g, '');
+        highlightIndex = marginLeftPosition / imgWidthRuleUnitValue + 2;
+        highlighterIndex(highlightIndex);
+      }
+    });
+  });
 }
 
 function highlighterIndex(element) {
@@ -112,3 +116,26 @@ function highlighterIndex(element) {
     }
   }
 }
+
+function GetStyle(CLASSname) {
+  var styleSheets = document.styleSheets;
+  var styleSheetsLength = styleSheets.length;
+  for (var i = 0; i < styleSheetsLength; i++) {
+    if (styleSheets[i].rules) { var classes = styleSheets[i].rules; }
+    else {
+      try { if (!styleSheets[i].cssRules) { continue; } }
+      //Note that SecurityError exception is specific to Firefox.
+      catch (e) { if (e.name == 'SecurityError') { console.log("SecurityError. Cant readd: " + styleSheets[i].href); continue; } }
+      var classes = styleSheets[i].cssRules;
+    }
+    for (var x = 0; x < classes.length; x++) {
+      if (classes[x].selectorText == CLASSname) {
+        var ret = (classes[x].cssText) ? classes[x].cssText : classes[x].style.cssText;
+        if (ret.indexOf(classes[x].selectorText) == -1) { ret = classes[x].selectorText + "{" + ret + "}"; }
+        return ret;
+      }
+    }
+  }
+}
+
+//https://stackoverflow.com/questions/324486/how-do-you-read-css-rule-values-with-javascript
